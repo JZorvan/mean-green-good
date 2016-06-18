a2d4d3e342178cb280b10b0fa2fc7dd4adb84c5b
Janelle Zorvan <jzorvan@gmail.com>
2016-06-18 09:52:43 -0500

    Auth is completed, starter stock recipe json added

--- app/app.js
+++ app/app.js
@@ -1,3 +1,36 @@
 "use strict";
 
-var app = angular.module("MeanGreenApp", ["ngRoute"]);
+var app = angular.module("MeanGreenApp", ["ngRoute"])
+  .constant("firebaseURL", "https://meangreengood.firebaseio.com/");
+
+
+let isAuth = (AuthFactory) => new Promise((resolve, reject) => {
+  if (AuthFactory.isAuthenticated()) {
+    console.log("isAuth");
+
+    resolve();
+  } else {
+    reject();
+  }
+});
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl: "partials/logscreen.html",
+      controller: "LogCtrl",
+      resolve: {isAuth}
+    })
+    .when("/login", {
+      templateUrl: "partials/logscreen.html",
+      controller:  "LogCtrl"
+    })
+    .when("/register", {
+      templateUrl: "partials/logscreen.html",
+      controller:  "LogCtrl"
+    })
+    .when("/logout",{
+      templateUrl: "partials/logscreen.html",
+      controller:  "LogCtrl"
+    })
+});
---
+++ app/controllers/LogCtrl.js
@@ -0,0 +1,56 @@
+"use strict";
+
+app.controller('LogCtrl', function ($scope, $location, $rootScope, firebaseURL, AuthFactory) {
+
+  let ref = new Firebase(firebaseURL);
+
+  if($location.path() === "/login"){
+      $rootScope.modeLogin = true;
+  };
+
+  if($location.path() === "/register"){
+      $rootScope.modeLogin = false;
+  };
+
+  if($location.path() === "/logout"){
+    ref.unauth();
+    $rootScope.isActive = false;
+  };
+
+  $rootScope.account = {
+    email: "",
+    password: ""
+  };
+
+  $scope.register = (AuthFactory) => {
+    ref.createUser({
+      email: $rootScope.account.email,
+      password: $rootScope.account.password
+    }, (error, userData) => {
+      if (error) {
+        $scope.errorMessage = error.message;
+        $scope.userError = true;
+        $scope.$apply();
+      } else if (userData) {
+        $scope.login();
+      }
+    });
+  };
+
+  $scope.login = () => {
+    AuthFactory
+      .authenticate($rootScope.account)
+        .then((userCreds) => {
+          $scope.$apply(function() {
+            $location.path("/userHome");
+            $rootScope.isActive = true;
+          });
+        })
+        .catch((error) => {
+          $scope.errorMessage = error.message;
+          $scope.userError = true;
+          $scope.$apply();
+        });
+  };
+
+});
